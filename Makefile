b79e04f127786dc0b98a1b543130ef46f94ddcae
thedpaul <dpaul2143@gmail.com>
2024-07-23 12:34:38 +0200

    Added simple linked list utility library

--- Makefile
+++ Makefile
@@ -1,15 +1,35 @@
 CFLAGS = -pedantic -Wall
 
-.PHONY: all
-
+.PHONY: all debug sanitize clean test
 all: mazer
 
+test: CFLAGS += -g
+test: test_linked_list
+	./test_linked_list
+
+debug: CFLAGS += -g
+debug: all
+
+sanitize: CFLAGS += -fsanitize=address
+sanitize: LDFLAGS += -lasan
+sanitize: all
+
 mazer: main.o maze.o
-	gcc $^ -o $@
+	$(CC) $^ -o $@
 
 main.o: main.c maze.h
-	gcc -c $(CFLAGS) $<
+	$(CC) -c $(CFLAGS) $<
 
 maze.o: maze.c maze.h
-	gcc -c $(CFLAGS) $<
+	$(CC) -c $(CFLAGS) $<
 
+test_linked_list: test_linked_list.o linked_list.o
+	$(CC) $^ -o $@ $(LDFLAGS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	$(RM) -f *.o
+	$(RM) -f mazer
+	$(RM) -f test_linked_list
---
+++ linked_list.c
@@ -0,0 +1,115 @@
+#include "linked_list.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct ll_node {
+    void* data;
+    struct ll_node* next;
+} ll_node;
+
+const ll_node LL_TAIL_INITIALIZER = {NULL, NULL};
+
+struct linked_list {
+    ll_node head;
+    ll_node* tail;
+};
+
+linked_list* llCreate(void) {
+    linked_list* list = malloc(sizeof(*list));
+    if (NULL != list) {
+        list->tail = &list->head;
+        *list->tail = LL_TAIL_INITIALIZER;
+    }
+
+    return list;
+}
+
+void llDestroy(linked_list* list) {
+    while (list->tail != &list->head) {
+        ll_node* next = list->head.next;
+        list->head.data = next->data;
+        list->head.next = next->next;
+        free(next);
+    }
+    free(list);
+}
+
+int llPushFront(linked_list* list, void* data) {
+    ll_node* node = malloc(sizeof(*node));
+    if (NULL != node) {
+        *node = list->head;
+        list->head.data = data;
+        list->head.next = node;
+
+        if (list->tail == &list->head) {
+            list->tail = node;
+        }
+
+        return 0;
+    }
+
+    return -1;
+}
+
+int llPushBack(linked_list* list, void* data) {
+    ll_node* node = malloc(sizeof(*node));
+    if (NULL != node) {
+        *node = LL_TAIL_INITIALIZER;
+
+        list->tail->data = data;
+        list->tail->next = node;
+        
+        list->tail = node;
+
+        return 0;
+    }
+
+    return -1;
+}
+
+int llPopFront(linked_list* list, void** pdata) {
+    if (list->tail != &list->head) {
+        ll_node* next = list->head.next;
+        *pdata = list->head.data;
+        list->head.data = next->data;
+        list->head.next = next->next;
+        free(next);
+
+        if (NULL == list->head.next) {
+            list->tail = &list->head;
+        }
+
+        return 0;
+    }
+
+    return -1;
+}
+
+int llPopBack(linked_list* list, void** pdata) {
+    if (list->tail != &list->head) {
+        ll_node* prev = &list->head;
+        while (prev->next != list->tail) {
+            prev = prev->next;
+        }
+        *pdata = prev->data;
+        *prev = *list->tail;
+        free(list->tail);
+        list->tail = prev;
+
+        return 0;
+    }
+
+    return -1;
+}
+
+void llPrint(linked_list* list) {
+    ll_node* node = &list->head;
+    while (node != list->tail) {
+        if (printf("%p\n", node->data) < 0)
+            abort();
+        node = node->next;
+    }
+    if (printf("\n") < 0)
+        abort();
+}
